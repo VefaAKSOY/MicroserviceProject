5b4ffc0f2df7735db7d1c1b8033cae7d0b29c42c
Bayram Vefa AKSOY <bayramvefa.aksoy@gmail.com>
2022-08-22 14:27:34 +0300

    Data Layer has been created

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,19 @@
+﻿using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    { 
+        public CatalogContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.GetValue<string>("DatabaseStrings:ConnectionString"));
+            var database = client.GetDatabase(configuration.GetValue<string>("DatabaseStrings:DatabaseName"));
+
+            Products = database.GetCollection<Product>(configuration.GetValue<string>("DatabaseStrings:CollectionName"));
+            CatalogContextSeed.SeedData(Products);
+        }
+        public IMongoCollection<Product> Products { get; }
+    }
+}
